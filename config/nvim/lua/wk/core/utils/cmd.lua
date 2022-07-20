48de34a40c8c20c751958c59763c30f1000d522d
Wojciech Kania <wojciech.kania@me.com>
2022-07-20 18:00:59 +0200

    nvim: map create command

---
+++ config/nvim/lua/wk/core/utils/cmd.lua
@@ -0,0 +1,34 @@
+local log = require("wk.core.utils.log")
+
+
+local function validate_command(name, command, opts)
+	vim.validate({
+		name = {name, 'string'},
+		command = {command, {'string', 'function'}},
+
+		opts = {opts, {'table'}, true}
+	})
+end
+
+
+
+local function cmd(name, command, opts)
+	local ok, errmsg = pcall(validate_command, name, command, opts)
+
+	if not ok then
+		log.err(errmsg)
+		return
+	end
+
+	opts = opts or {}
+
+	ok, errmsg = pcall(vim.api.nvim_create_user_command, name, command, opts)
+
+	if not ok then
+		log.err(errmsg)
+	end
+
+end
+
+
+return cmd
