5aa4453ca10c7fdfd6e3765888cbbe3f3aabc342
Wojciech Kania <wojciech.kania@me.com>
2022-07-20 18:05:25 +0200

    nvim: reload config function

---
+++ config/nvim/lua/wk/core/reload.lua
@@ -0,0 +1,14 @@
+local log = require("wk.core.utils.log");
+
+local function reload_nvim_config()
+	for name, _ in pairs(package.loaded) do
+		if name:match('wk') then
+			package.loaded[name] = nil
+		end
+	end
+
+	dofile(vim.env.MYVIMRC)
+	log.info("Nvim configuration reloaded")
+end
+
+return reload_nvim_config
