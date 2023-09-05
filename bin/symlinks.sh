0068d3271ffad128f1592f5933224871baacbb98
Wojciech Kania <wojtek@kania.sh>
2023-09-05 22:21:46 +0200

    alacritty font settings and add symlinks ~/.config

--- bin/symlinks.sh
+++ bin/symlinks.sh
@@ -3,3 +3,16 @@
 
 # create symlink to keymap configuration
 ln -s ~/.dotfiles/macos/keymap.xml ~/Library/LaunchAgents/com.local.KeyRemapping.plist
+
+# dotfiles directory
+dir=$HOME/.dotfiles 
+
+# symlinks ~/.config
+[ -d ~/.config ] || mkdir ~/.config
+for file in "$dir/config/"*; do
+    filename=$(echo $file | rev | cut -d'/' -f-1 | rev)
+    if [ ! -e ~/.config/$filename ]; then
+        echo "Creating symlink to $filename in .config directory."
+        ln -s -f $dir/config/$filename ~/.config/$filename
+    fi
+done
