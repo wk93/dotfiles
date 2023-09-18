15d0c7cc467dfd77c8a534a73ff80312f6f2fb6b
Wojciech Kania <wojtek@kania.sh>
2023-09-18 16:26:25 +0200

    [nvim] line numbers and indent config

--- config/nvim/lua/wk/core/config.lua
+++ config/nvim/lua/wk/core/config.lua
@@ -1,3 +1,16 @@
 vim.g.mapleader = " "
 
 vim.o.termguicolors = true
+
+-- numbers
+vim.opt.number = true
+vim.opt.numberwidth = 2
+vim.opt.relativenumber = true
+vim.opt.ruler = true
+
+-- indenting
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.smartindent = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
