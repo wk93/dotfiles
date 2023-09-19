d74ffb71b403a1a1523a671375f9c6c1d555c51a
Wojciech Kania <wojtek@kania.sh>
2023-09-19 11:01:16 +0200

    add neodev.nvim

--- config/nvim/lua/wk/plugins/lsp/lspconfig.lua
+++ config/nvim/lua/wk/plugins/lsp/lspconfig.lua
@@ -2,4 +2,12 @@ return {
   "neovim/nvim-lspconfig",
   event = { "BufReadPost", "BufNewFile" },
   cmd = { "LspInfo", "LspInstall", "LspUninstall" },
+  dependencies = { "folke/neodev.nvim", opts = {} },
+  config = function ()
+    require("neodev").setup({})
+
+    local lspconfig = require("lspconfig")
+    lspconfig.lua_ls.setup({})
+
+  end
 }
