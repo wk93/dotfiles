9fece48f73ba95ef6f98d043dd699db55325d3e2
Wojciech Kania <wojtek@kania.sh>
2023-09-19 12:07:52 +0200

    [nvim] keybindigs for lsp

--- config/nvim/lua/wk/plugins/lsp/lspconfig.lua
+++ config/nvim/lua/wk/plugins/lsp/lspconfig.lua
@@ -3,11 +3,25 @@ return {
   event = { "BufReadPost", "BufNewFile" },
   cmd = { "LspInfo", "LspInstall", "LspUninstall" },
   dependencies = { "folke/neodev.nvim", opts = {} },
-  config = function ()
+  config = function()
     require("neodev").setup({})
 
     local lspconfig = require("lspconfig")
-    lspconfig.lua_ls.setup({})
 
-  end
+    local keybinds = {
+      ["gR"] = { "<cmd>Telescope lsp_references<CR>", "show definition, references" },
+      ["gD"] = { vim.lsp.buf.declaration, "show definition, references" },
+      ["gd"] = { "<cmd>Telescope lsp_definitions<CR>", "show lsp definitions" },
+      ["gi"] = { "<cmd>Telescope lsp_implementations<CR>", "show lsp implementations" },
+      ["gt"] = { "<cmd>Telescope lsp_type_definitions<CR>", "show lsp type definitions" },
+      ["<leader>rn"] = { vim.lsp.buf.rename, "smart rename" },
+      ["<leader>D"] = { "<cmd>Telescope diagnostics bufnr=0<CR>", "show diagnostics for file" },
+      ["<leader>d"] = { vim.diagnostic.open_float, "show diagnostics for line" },
+      ["K"] = { vim.lsp.buf.hover, "show documentation for what is under cursor" },
+    }
+
+    utils.map("n", keybinds)
+
+    lspconfig.lua_ls.setup({})
+  end,
 }
