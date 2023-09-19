3408412598608b88a113b75ded06d0c5b751e084
Wojciech Kania <wojtek@kania.sh>
2023-09-19 11:12:43 +0200

    [nvim] treesitter

---
+++ config/nvim/lua/wk/plugins/treesitter.lua
@@ -0,0 +1,18 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  event = { "BufReadPre", "BufNewFile" },
+  build = ":TSUpdate",
+  config = function()
+    local treesitter = require("nvim-treesitter.configs")
+    treesitter.setup({
+      highlight = {
+        enable = true,
+      },
+      indent = { enable = true },
+      ensure_installed = {
+        "lua"
+      },
+      auto_install = true
+    })
+  end
+}
