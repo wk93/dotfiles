a2baa0ba8e8b0200d9dd5c217cea7c55f6b63b52
Wojciech Kania <wojtek@kania.sh>
2023-09-26 10:47:00 +0200

    use catppuccin macchiato theme

--- config/nvim/lua/wk/plugins/theme.lua
+++ config/nvim/lua/wk/plugins/theme.lua
@@ -1,10 +1,10 @@
 return {
-	{
-		"nordtheme/vim",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			vim.cmd([[colorscheme nord]])
-		end,
-	},
+  {
+    "catppuccin/nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd([[colorscheme catppuccin-macchiato]])
+    end,
+  },
 }
