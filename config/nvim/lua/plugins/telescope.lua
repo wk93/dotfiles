3b7fc0a583fb33409364b144e9fa134e043b5904
Wojciech Kania <wojtek@kania.sh>
2024-06-03 14:39:53 +0200

    [nvim] add fzf to telescope

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -1,6 +1,12 @@
 return {
 	"nvim-telescope/telescope.nvim",
-	dependencies = { "nvim-lua/plenary.nvim" },
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{
+			"nvim-telescope/telescope-fzf-native.nvim",
+			build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+		},
+	},
 	keys = {
 		{
 			"<leader>ff",
@@ -19,6 +25,19 @@ return {
 			function()
 				require("telescope.builtin").buffers()
 			end,
+		},
+	},
+	config = function()
+		require("telescope").load_extension("fzf")
+	end,
+	opts = {
+		extensions = {
+			fzf = {
+				fuzzy = true,
+				override_generic_sorter = true,
+				override_file_sorter = true,
+				case_mode = "ignore_case",
+			},
 		},
 	},
 }
