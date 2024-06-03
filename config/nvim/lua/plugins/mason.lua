9dfa9cbc907edc5097273612e1f8f56811c4ec35
Wojciech Kania <wojtek@kania.sh>
2024-06-03 13:11:21 +0200

    [nvim] add mason

---
+++ config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,12 @@
+return {
+	"williamboman/mason.nvim",
+	cmd = "Mason",
+	keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
+	build = ":MasonUpdate",
+	opts = {
+		ensure_installed = {
+			"lua-language-server",
+			"stylua",
+		},
+	},
+}
