128e8c607d9c9a1aa461e9ea68f115ce67030388
Wojciech Kania <wojtek@kania.sh>
2024-06-03 13:15:14 +0200

    [nvim] add telescope

---
+++ config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,24 @@
+return {
+	"nvim-telescope/telescope.nvim",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	keys = {
+		{
+			"<leader>ff",
+			function()
+				require("telescope.builtin").find_files()
+			end,
+		},
+		{
+			"<leader>fg",
+			function()
+				require("telescope.builtin").live_grep()
+			end,
+		},
+		{
+			"<leader>fb",
+			function()
+				require("telescope.builtin").buffers()
+			end,
+		},
+	},
+}
