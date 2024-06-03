23f944249e71d5afffeb6bda53c55275ad669215
Wojciech Kania <wojtek@kania.sh>
2024-06-03 15:05:00 +0200

    [nvim] telescope ui-select

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -6,6 +6,7 @@ return {
 			"nvim-telescope/telescope-fzf-native.nvim",
 			build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
 		},
+		"nvim-telescope/telescope-ui-select.nvim",
 	},
 	keys = {
 		{
@@ -29,6 +30,7 @@ return {
 	},
 	config = function()
 		require("telescope").load_extension("fzf")
+		require("telescope").load_extension("ui-select")
 	end,
 	opts = {
 		extensions = {
