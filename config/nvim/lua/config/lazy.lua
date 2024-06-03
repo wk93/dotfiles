f530dca66123df8e05cd5b27d09df4e77544e5e0
Wojciech Kania <wojtek@kania.sh>
2024-06-03 12:29:50 +0200

    [nvim] add tokyo night theme

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -11,4 +11,9 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins")
+require("lazy").setup("plugins", {
+	install = {
+		missing = true,
+		colorscheme = { "tokyonight" },
+	},
+})
