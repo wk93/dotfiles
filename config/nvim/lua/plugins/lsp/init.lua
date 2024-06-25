df88a9615798a946355af84413793bd4a50d906d
Wojciech Kania <wojtek@kania.sh>
2024-06-25 09:24:21 +0200

    [nvim] add tailwind lsp

--- config/nvim/lua/plugins/lsp/init.lua
+++ config/nvim/lua/plugins/lsp/init.lua
@@ -10,5 +10,6 @@ return {
 		require("plugins.lsp.lua_ls").setup()
 		require("plugins.lsp.rust_analyzer").setup()
 		require("plugins.lsp.tsserver").setup()
+		require("lspconfig").tailwindcss.setup({})
 	end,
 }
