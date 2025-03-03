299f0e1a4859e524442ef8f83357d9267b18eb48
Wojciech Kania <wojtek@kania.sh>
2025-03-03 21:11:44 +0100

    nix settings

--- nixos/nix/default.nix
+++ nixos/nix/default.nix
@@ -1,27 +1,8 @@
-# This is your system's configuration file.
-# Use this to configure your system environment (it replaces /etc/nixos/configuration.nix)
 {
-  inputs,
   lib,
   config,
-  ...
+  inputs,
 }: {
-  # You can import other NixOS modules here
-  imports = [
-    # If you want to use modules from other flakes (such as nixos-hardware):
-    # inputs.hardware.nixosModules.common-cpu-amd
-    # inputs.hardware.nixosModules.common-ssd
-
-    # You can also split up your configuration and import pieces of it here:
-    # ./users.nix
-
-    # Import your generated (nixos-generate-config) hardware configuration
-
-    ./system
-    ./boot
-    ./hardware-configuration.nix
-  ];
-
   nixpkgs = {
     # You can add overlays here
     overlays = [
@@ -59,22 +40,5 @@
     # Opinionated: make flake registry and nix path match flake inputs
     registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
     nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
-  };
-
-  # FIXME: Add the rest of your current configuration
-
-  networking.hostName = "t480";
-
-  # This setups a SSH server. Very important if you're setting up a headless system.
-  # Feel free to remove if you don't need it.
-  services.openssh = {
-    enable = true;
-    settings = {
-      # Opinionated: forbid root login through SSH.
-      PermitRootLogin = "no";
-      # Opinionated: use keys only.
-      # Remove if you want to SSH using passwords
-      PasswordAuthentication = false;
-    };
   };
 }
