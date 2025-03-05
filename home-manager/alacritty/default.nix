c2697207ae31bc24e26b340d27e311b94d300600
Wojciech Kania <wojtek@kania.sh>
2025-03-05 10:04:23 +0100

    alacritty

---
+++ home-manager/alacritty/default.nix
@@ -0,0 +1,22 @@
+{
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      font = {
+        size = 12;
+        normal = {
+          family = "Fira Code Nerd Font";
+          style = "Regular";
+        };
+        bold = {
+          family = "FiraCode Nerd Font";
+          style = "Bold";
+        };
+        italic = {
+          family = "FiraCode Nerd Font";
+          style = "Italic";
+        };
+      };
+    };
+  };
+}
--- home-manager/home.nix
+++ home-manager/home.nix
@@ -14,6 +14,7 @@
     ./hyprland
     ./git
     ./ssh
+    ./alacritty
     ./catppuccin
   ];
 
