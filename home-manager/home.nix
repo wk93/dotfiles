d41ba5f18e41f0a08d0bf56003c530c0360d93c5
Wojciech Kania <wojtek@kania.sh>
2025-03-05 10:00:05 +0100

    remove git.enable from home.nix

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -51,9 +51,7 @@
     _1password-gui
   ];
 
-  # Enable home-manager and git
   programs.home-manager.enable = true;
-  programs.git.enable = true;
 
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
